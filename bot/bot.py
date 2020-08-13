de923164ea02691f2072b675a0c2652472e07a92
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2020-08-13 14:36:12 +0300

    Fix typo

--- bot/bot.py
+++ bot/bot.py
@@ -92,7 +92,7 @@ async def requests(ctx):
 
     with open('temp.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(("Video Title", "Video URL","Play Count","Played Mostly By"))
+        writer.writerow(("Video Title", "Video URL","Play Count","Played Mostlys By"))
         for request in request_list:
             tmp = list(request)
             user = client.get_user(tmp[3])
